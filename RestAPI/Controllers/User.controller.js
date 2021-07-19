8e746e39d412aece10a890c82b40277463206f46
Sagar Suman <sagarsuman1299@gmail.com>
2021-07-19 13:16:27 +0530

    fix/user_api

--- RestAPI/Controllers/User.controller.js
+++ RestAPI/Controllers/User.controller.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+_ = require("lodash");
 const createError = require("http-errors");
 const mongoose = require("mongoose");
 const User = require("../Models/User.model");
@@ -11,8 +13,11 @@ module.exports = {
       }
 
       const user = new User(req.body);
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(user.password, salt);
       const result = await user.save();
-      res.send(result);
+      res.send(_.pick(result, ["_id", "name", "email"]));
     } catch (error) {
       console.log(error.message);
       if (error.name === "ValidationError") {
@@ -23,9 +28,19 @@ module.exports = {
   },
   loginUser: async (req, res, next) => {
     try {
-      const user = new User(req.body);
-      const result = await user.save();
-      res.send(result);
+      let userDuplication = await User.findOne({ email: req.body.email });
+      if (!userDuplication) {
+        return res.status(400).send("User donot exists.");
+      }
+      //
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        userDuplication.password
+      );
+      if (!validPassword) {
+        return res.status(400).send("Invalid Email or Password");
+      }
+      res.send(userDuplication.id);
     } catch (error) {
       console.log(error.message);
       if (error.name === "ValidationError") {
@@ -41,7 +56,7 @@ module.exports = {
       if (!user) {
         throw createError(404, "User donot exists");
       }
-      res.send(user);
+      res.send(_.pick(user, ["name", "email"]));
     } catch (error) {
       console.log(error.message);
       if (error instanceof mongoose.CastError) {
@@ -57,11 +72,14 @@ module.exports = {
       const update = req.body;
       const options = { new: true };
 
+      const salt = await bcrypt.genSalt(10);
+      update.password = await bcrypt.hash(update.password, salt);
+
       const result = await User.findByIdAndUpdate(id, update, options);
       if (!result) {
         throw createError(404, "User does not exist");
       }
-      res.send(result);
+      res.send("success");
     } catch (error) {
       console.log(error.message);
       if (error instanceof mongoose.CastError) {
